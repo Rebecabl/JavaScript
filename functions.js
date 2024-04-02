1a37f055855cce55c02d5e334052c1d2c4a2d68a
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2024-04-02 15:48:01 -0300

    Delete functions.js

--- functions.js
+++
@@ -1,7 +0,0 @@
-function splitAndSort() {
-    let input = document.getElementById("input").value;
-    let listArray = input.split(",").map(item => item.trim());
-    listArray.sort();
-    let output = listArray.join(", ");
-    document.getElementById("result").innerText = output;
-}
